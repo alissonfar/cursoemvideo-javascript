9c0ce02c4eb3c39fa6e7c3e7f4b6a9370e492031
alissonfar <alissonfariascamargo@gmail.com>
2024-04-01 07:20:16 -0300

    Primeiro exercício finalizado

--- aula 13 curso em vídeo exercícios/script.js
+++ aula 13 curso em vídeo exercícios/script.js
@@ -1,1 +1,23 @@
-alert('Olá!')
+
+
+
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    // var hora = 22
+    
+    msg.innerHTML = `Agora são ${hora} horas.`
+    
+    if (hora > 0 && hora <=12) {
+        img.src = 'images/manha circular.png'
+        window.document.body.style.background = '#e2cd9f'
+    } else if (hora > 12 && hora < 18){
+        img.src = 'images/tarde circular.png'
+        window.document.body.style.background = '#b9846f'
+    } else {
+        img.src = 'images/noite circular.png'
+        document.body.style.background = '#515154'
+    }
+}
